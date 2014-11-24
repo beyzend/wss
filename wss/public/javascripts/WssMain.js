792e9dc90d99d4a1fcd05f5a91b34e6131c7c19d
Beyzend <llwijk@gmail.com>
2014-11-23 16:10:38 -0800

    added scrolling to tile map.

--- wss/public/javascripts/WssMain.js
+++ wss/public/javascripts/WssMain.js
@@ -1,48 +1,75 @@
-var onInit = function() {
-   
+$(function() {
+  
     var tileAnims = [];
     // Figure out something cute and functional tomorrow here.
-    for ( y = 0; y < 16; ++y )
-    {
-        for( x = 0; x < 16; ++x )
-        {
-            tileAnims.push( new $.gQ.Animation( {
-                imageURL: "images/art/ironhand.png",
-                numberOfFrame: 1,
-                delta: 18,
-                distance: 0,
-                type: $.gQ.ANIMATION_MULTI | $.gQ.ANIMATION_HORIZONTAL | $.gQ.ANIMATION_ONCE,
-                offsetx: x * 18,
-                offsety: y * 18
+    for ( y = 0; y < 16; ++y) {
+        for ( x = 0; x < 16; ++x) {
+            tileAnims.push(new $.gQ.Animation({
+                imageURL : "images/art/ironhand.png",
+                numberOfFrame : 1,
+                delta : 18,
+                distance : 0,
+                type : $.gQ.ANIMATION_MULTI | $.gQ.ANIMATION_HORIZONTAL | $.gQ.ANIMATION_ONCE,
+                offsetx : x * 18,
+                offsety : y * 18
             }));
         }
     }
+
+    var idx = 0;
+    var tileDef = Array.apply(null, {
+        length : 100
+    }).map(function() {
+        return Array.apply(null, {
+            length : 100
+        }).map(function() {
+            return idx++ % 5;
+        });
+    });
+
+    var anims = [];
+
+    $("#gamescreen").playground({
+        width : 640,
+        height : 480,
+        refreshRate : 60,
+        keyTracker: true
+    })
+    //.addGroup( "mapTiles", {width:640, height:480} )
+    //.importTilemaps( "images/art/test.json" );
     
-    var tileDef = Array.apply( null, {length:25}).map( function(){
-       return Array.apply( null, {length:25} ).map( function() {
-           return 2;
-       });
+    .addTilemap("mapTiles", tileDef, tileAnims, {
+        width : 18,
+        height : 18,
+        sizex : 100,
+        sizey : 100
     });
     
+
     
-    var anims = [];
-    
-    $("#gamescreen").playground({
-        width: 640,
-        height: 480,
-        refreshRate: 60
-    })
-    //.importTilemaps( "images/art/test.json" );
-    .addTilemap( "basicTile", tileDef, tileAnims,
-        {
-           width: 18,
-           height: 18,
-           sizex: 25,
-           sizey: 25 
-        });
-    
-    $.playground().startGame( function(){});
-    
-  
-    
-};
+
+    $( document ).keydown( function( e ) {
+       
+       var map = $( "#mapTiles" );   
+       
+       switch ( e.keyCode ) {
+           case 65: // this is left
+            map.x( -1, true );
+            break;
+           case 87: // this is up
+            map.y( -1 , true );
+            break;
+           case 68: // this is right
+            map.x( 1, true );
+            console.log("going right!");
+            break;
+           case 83: // this is down
+            map.y( 1, true );
+            break;
+       } 
+    });
+
+    $.playground().startGame(function() {
+    });
+}); 
+
