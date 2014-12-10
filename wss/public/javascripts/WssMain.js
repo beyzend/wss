75dcee6d273713019eb181ebc430614995ac9687
beyzend <llwijk@gmail.com>
2014-12-10 11:41:50 -0800

    added initial Map module for RequireJS. Added loading of json map file from socket.io.

--- wss/public/javascripts/WssMain.js
+++ wss/public/javascripts/WssMain.js
@@ -1,12 +1,11 @@
-
 // void ComputeBlockXY(float uvX, out float2 blockCoords)
 // {
-  // float idx = uvX * 256.0 - 1;
-  // blockCoords.y = floor(idx / 16.0);
-  // blockCoords.x = (idx - blockCoords.y * 16.0);
+// float idx = uvX * 256.0 - 1;
+// blockCoords.y = floor(idx / 16.0);
+// blockCoords.x = (idx - blockCoords.y * 16.0);
 // }
 
-function Entity( id , animation ) {
+function Entity(id, animation) {
     this.id = id;
     this.animation = animation;
 }
@@ -14,47 +13,10 @@ function Entity( id , animation ) {
 var AREA_WIDTH = 640;
 var AREA_HEIGHT = 480;
 
-function loadMap( jsonMap ) {
-    
-    var tileAnims = [];
-    
-    for ( y = 0; y < 16; ++y) {
-        for ( x = 0; x < 16; ++x) {
-            tileAnims.push(new $.gQ.Animation({
-                imageURL : "images/art/ironhand.png",
-                numberOfFrame : 1,
-                delta : 18,
-                distance : 0,
-                type : $.gQ.ANIMATION_MULTI | $.gQ.ANIMATION_HORIZONTAL | $.gQ.ANIMATION_ONCE,
-                offsetx : x * 18,
-                offsety : y * 18
-            }));
-        }
-    }
+require(["/javascripts/map.js"], function(Map) {
 
-    var idx = 0;
-    
-    var tileDef = Array.apply(null, {
-        length : 100
-    }).map(function() {
-        return Array.apply(null, {
-            length : 100
-        }).map(function() {
-            return idx++ % 5;
-        });
-    });
-    
-    mapTiles.addTilemap("map", tileDef, tileAnims, {
-         width : 18,
-         height : 18,
-         sizex : 100,
-         sizey : 100
-    }).end();
-    
-}
+    $.data["map"] = Map;
 
-require([], function(){
-    
     var playerAnimation = new $.gQ.Animation({
         imageURL : "images/art/ironhand.png",
         numberOfFrame : 1,
@@ -64,55 +26,73 @@ require([], function(){
         offsetx : 1 * 18,
         offsety : 0 * 18
     });
-    
-    var playerEntity = new Entity( 1, playerAnimation );
-    
-   $("#gamescreen").playground({
+
+    var playerEntity = new Entity(1, playerAnimation);
+
+    $("#gamescreen").playground({
         width : AREA_WIDTH,
         height : AREA_HEIGHT,
         refreshRate : 60,
-        keyTracker: true
-   })
-   .addGroup( "mapTiles", { width:640, height:480} )
-     .importTilemaps( "images/art/test.json" )
-   .end()  
-   .addGroup( "player", {posx: AREA_WIDTH / 2, posy: AREA_HEIGHT / 2, width: 18, height: 18 } )
-        .addSprite( "player", { animation: playerAnimation , width: 18, height: 18 } )
-   .end();
-   
-    var socket = io.connect( 'http://localhost:3000' );
-    socket.on( 'news', function( data ) {
-        console.log( data );
-        socket.emit( 'my other event', { my: 'data' });
-    });
-    var mapTiles = $( "#mapTiles" );
-    socket.on( 'map', function( data ) {
-        // pretend to load map right now. We need to manually load data.map.
-        console.log( data.map );
-             
-    });
-  
-    $( document ).keydown( function( e ) {
-       
-       var map = $( "#mapTiles" );   
-       
-       switch ( e.keyCode ) {
-           case 65: // this is left
-            map.x( 1, true );
+        keyTracker : true
+    }).addGroup("mapTiles", {
+        width : 640,
+        height : 480
+    })
+    //.importTilemaps( "images/art/test.json" )
+    .end().addGroup("player", {
+        posx : AREA_WIDTH / 2,
+        posy : AREA_HEIGHT / 2,
+        width : 18,
+        height : 18
+    }).addSprite("player", {
+        animation : playerAnimation,
+        width : 18,
+        height : 18
+    }).end();
+
+    
+    $(document).keydown(function(e) {
+
+        var map = $("#mapTiles");
+
+        switch ( e.keyCode ) {
+        case 65:
+            // this is left
+            map.x(1, true);
             break;
-           case 87: // this is up
-            map.y( 1 , true );
+        case 87:
+            // this is up
+            map.y(1, true);
             break;
-           case 68: // this is right
-            map.x( -1, true );
+        case 68:
+            // this is right
+            map.x(-1, true);
             console.log("going right!");
             break;
-           case 83: // this is down
-            map.y( -1, true );
+        case 83:
+            // this is down
+            map.y(-1, true);
             break;
-       } 
+        }
     });
 
     $.playground().startGame(function() {
+        var socket = io.connect('http://localhost:3000');
+        socket.on('news', function(data) {
+            console.log(data);
+            socket.emit('my other event', {
+                my : 'data'
+            });
+        });
+        var mapTiles = $("#mapTiles");
+        socket.on('map', function(data) {
+            // pretend to load map right now. We need to manually load data.map.
+            console.log(data.map);
+            var mapGroup = $("#mapTiles");
+            var map = $.data["map"];
+            map.addTilemapToGroup(mapGroup, "testmap", data.map);
+
+        });
     });
+
 });
--- wss/public/javascripts/map.js
+++ wss/public/javascripts/map.js
@@ -1,3 +1,58 @@
 /**
  * @author beyzend
  */
+define( function () {
+   
+   // gets Tile Animations 
+   function getTileAnimations( tileset ) {
+       
+       var tileAnims = [];
+       var atlasHeight = tileset.imageheight / tileset.tileheight;
+       var atlasWidth = tileset.imagewidth / tileset.tilewidth;
+       
+       for ( y = 0; y < atlasHeight; ++y ) {
+           for ( x = 0; x < atlasWidth; ++x ) {
+               tileAnims.push( new $.gQ.Animation( {
+                   imageURL : tileset.image,
+                   numOfFrame : 1,
+                   delta : tileset.tilewidth,
+                   distance : 0,
+                   type : $.gQ.ANIMATION_MULTI | $.gQ.ANIMATION_HORIZONTAL | $.gQ.ANIMATION_ONCE,
+                   offsetx : x * tileset.tilewidth,
+                   offsety : y * tileset.tileheight
+               }));
+           }
+       }
+       return tileAnims;
+   }
+   
+   function getLayerDefsFromLayer( layer ) {
+       
+       var layerDefs = [];
+       
+       for ( y = 0; y < layer.height; ++y ) {
+           var row = [];
+           for ( x = 0; x < layer.width; ++x ) {
+               row.push( layer.data[ y * layer.width + x ] );
+           }
+           layerDefs.push( row );
+       }
+       return layerDefs;
+   }
+   
+   function addTilemap( mapGroup, mapName, jsonMap ) {
+       var tileAnims = getTileAnimations( jsonMap.tilesets[ 0 ] );
+       var layerDefs = getLayerDefsFromLayer( jsonMap.layers[ 0 ] );
+       
+       mapGroup.addTilemap( mapName, layerDefs, tileAnims, {
+           width : jsonMap.tilewidth,
+           height : jsonMap.tileheight,
+           sizex : jsonMap.width,
+           sizey : jsonMap.height
+       });
+   }
+   
+   return {
+       addTilemapToGroup : addTilemap
+   }; 
+});
