b211c4fa55e02ebdfe284c088dfaca631523c567
beyzend <llwijk@gmail.com>
2014-12-11 08:55:54 -0800

    started testing messaging between front end and back end.

--- wss/gameserver/src/wssserver.cpp
+++ wss/gameserver/src/wssserver.cpp
@@ -4,29 +4,48 @@
 
 #include <zmqpp/zmqpp.hpp>
 
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
 
-int main ( int argc, char** argv )
-{
-  using namespace std;
-  cout << "FUCK YOU! TO DO: Use ZerMQPP binding" << endl;
-  
-  zmqpp::context context;
-  
-  zmqpp::socket server ( context, zmqpp::socket_type::push );
-  server.bind ( "tcp://127.0.0.1:4200" );
-  
-  zmqpp::socket client ( context, zmqpp::socket_type::pull );
-  client.connect( "tcp://127.0.0.1:4200" );
-  
-  zmqpp::message request;
-  request << "Hello";
-  server.send( request );
+#include <chrono>
+#include <thread>
 
-  zmqpp::message response;
-  client.receive( response );
 
-  assert( "Hello" == response.get( 0 ) );
-  cout << "Grasslands test OK" << endl;
-  
-  return 0;
+#define within(num) (int) ((float) num * random () / (RAND_MAX + 1.0))
+
+int main(int argc, char** argv) {
+	using namespace std;
+
+	cout << "Starting ZeroMQ server..." << endl;
+
+	zmqpp::context context;
+
+	zmqpp::socket server(context, zmqpp::socket_type::publish);
+	server.bind("tcp://127.0.0.1:4200");
+	server.bind("ipc://weather.ipc");
+
+	//zmqpp::socket client(context, zmqpp::socket_type::pull);
+	//client.connect("tcp://127.0.0.1:4200");
+
+	while( 1 ) {
+		int zipcode, temperature, relhumidity;
+
+		zipcode = within( 100000 );
+		temperature = within( 215 ) - 80;
+		relhumidity = within( 50 ) + 10;
+
+		zmqpp::message message;
+		message << "zipcode: " << zipcode << " temp: " << temperature << " relhumidity: " << relhumidity;
+
+		cout << "Sending message: zip,temp,hum: " << zipcode << "," << temperature << "," << relhumidity << endl;
+
+
+		server.send( message );
+
+
+		std::this_thread::sleep_for( std::chrono::milliseconds( 500 ) );
+	}
+
+	return 0;
 }
